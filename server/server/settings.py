5cb5a677f2df9597faef74028e9cbf7ac7bd4716
jonathan <zlatanjon@hotmail.com>
2022-02-07 00:01:56 -0600

    update settings db

--- server/server/settings.py
+++ server/server/settings.py
@@ -93,8 +93,19 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'dogger_db',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'postgres',
+
+        'HOST': '',
+
+        'PORT': '5432',
+
     }
 }
 
