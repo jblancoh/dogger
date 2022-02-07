7d53619a044fd3801cf28536467948f896f69bc0
jonathan <zlatanjon@hotmail.com>
2022-02-06 22:41:55 -0600

    modify requirements

--- server/server/settings.py
+++ server/server/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
@@ -138,3 +138,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/'
+
+django_heroku.settings(locals())
